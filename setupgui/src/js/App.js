7c84395fa8b4f60480604d0312549b1cc23e2c27
Kevin Hogan <kevin.hogan1992@gmail.com>
2019-02-26 23:43:06 -0500

    Adding nodes (kind of) working

--- setupgui/src/js/App.js
+++ setupgui/src/js/App.js
@@ -4,16 +4,35 @@ import CytoscapeComponent from 'react-cytoscapejs';
 class App extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+        node_count: 0
+    };
+
+    this.addNode = this.addNode.bind(this);
+  }
+
+  addNode() {
+    this.setState(state => ({
+      node_count: state.node_count + 1
+    }));
   }
 
   render(){
-    const elements = [
-       { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-       { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-       { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
+    const nexus = [
+      { data: { id: 'nexus', label: 'Switch' }, position: { x: 100, y: 100 } }
     ];
 
-    return <CytoscapeComponent elements={elements} style={ { width: '600px', height: '600px' } } />;
+    const nodes = [...Array(this.state.node_count).keys()].map(i => {
+      return [
+        { data: { id: i, label: 'Node ' + i }, position: { x: 200, y: 50 + i*(100/this.state.node_count) } },
+        { data: { source: 'nexus', target: i, label: '' } }
+      ];
+    }).flat();
+
+    return <div>
+            <CytoscapeComponent elements={nexus.concat(nodes)} style={ { width: '600px', height: '600px' } } />
+            <button onClick={this.addNode}>Add Node (Total: {this.state.node_count})</button>
+        </div>;
   }
 }
 

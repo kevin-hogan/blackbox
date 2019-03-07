22cdb6d70d100854969cbef3805ad6086eaa4e66
Kevin Hogan <kevin.hogan1992@gmail.com>
2019-03-07 12:55:43 -0500

    integrate conf json with topology and requests

--- start_mininet.py
+++ start_mininet.py
@@ -13,19 +13,19 @@ SNORT_HOST_NAME = "snort"
 WEBSERVER_HOST_NAME = "webserver"
 
 class SingleSwitchTopo(Topo):
-    def build(self, numClients):
+    def build(self, clientConfig):
         switch = self.addSwitch('s1')
         link_opts = dict(cls=TCLink, bw=100, delay='5ms', max_queue_size=1000, use_htb=True)
-        for num in range(numClients):
-            host = self.addHost('client%s' % (num + 1))
+        for client in clientConfig:
+            host = self.addHost(client["name"])
             self.addLink(host, switch, **link_opts)
         snort = self.addHost(SNORT_HOST_NAME, inNamespace=False)
         self.addLink(snort, switch, **link_opts)
         webserver = self.addHost(WEBSERVER_HOST_NAME, inNamespace=False)
         self.addLink(webserver, switch, **link_opts)
 
-def createNetwork(numClients):
-    topo = SingleSwitchTopo(numClients)
+def createNetwork(clientConfig):
+    topo = SingleSwitchTopo(clientConfig)
     net = Mininet(topo=topo, controller=None)
     controller = net.addController('c0', controller=RemoteController, ip="127.0.0.1", port=6633)
     controller.start()
@@ -38,17 +38,25 @@ def testConnection(net):
     print("Testing network connectivity")
     net.pingAll()
 
-if __name__ == '__main__':
-    with open('/src/conf.example.json') as f:
-        config = json.load(f)
-
-    setLogLevel('info')
-    net = createNetwork(len(config["clients"]))
-    testConnection(net)
+def startServicesOnHosts(net, config):
     net.get(SNORT_HOST_NAME).cmd("ifconfig snort-eth0 promisc")
     # The -q option, which silences output, is important below. Sending the command to the node
     # this way fails when it has output.
     net.get(SNORT_HOST_NAME).cmd("snort -q -i snort-eth0 -c /etc/snort/snort.conf -A unsock &")
     net.get(WEBSERVER_HOST_NAME).cmd("socat TCP4-LISTEN:80,fork TCP4:webserver:8080 &")
+
+    webserver_ip = net.get(WEBSERVER_HOST_NAME).IP()
+    for client in config["clients"]:
+        net.get(client["name"]).cmd("./src/client_scripts/" + client["request_script"] \
+            + " --server_ip " + webserver_ip + " &")
+
+if __name__ == '__main__':
+    with open('/src/conf.example.json') as f:
+        config = json.load(f)
+
+    setLogLevel('info')
+    net = createNetwork(config["clients"])
+    testConnection(net)
+    startServicesOnHosts(net, config)
     CLI(net)
     net.stop()

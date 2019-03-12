1436395cb150d12b6e9ed0ba3e03a4dc0d1cb1c0
Kevin Hogan <kevin.hogan1992@gmail.com>
2019-03-12 11:59:00 -0400

    turning off learning switch functionality temporarily

--- snort_learning_switch.py
+++ snort_learning_switch.py
@@ -58,10 +58,10 @@ class SnortLearningSwitch(app_manager.RyuApp):
         requests.post(url = "http://webserver:8080/alerts", data = data)
 
         # Add flow to drop packets from host that caused alert
-        pkt = packet.Packet(array.array('B', msg.pkt))
-        protocol = pkt.get_protocol(ipv4.ipv4)
-        match = parser.OFPMatch(ipv4_src=protocol.src, eth_type=0x800)
-        self.add_flow(self.datapath, 2, match, [])
+        # pkt = packet.Packet(array.array('B', msg.pkt))
+        # protocol = pkt.get_protocol(ipv4.ipv4)
+        # match = parser.OFPMatch(ipv4_src=protocol.src, eth_type=0x800)
+        # self.add_flow(self.datapath, 2, match, [])
 
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -134,16 +134,16 @@ class SnortLearningSwitch(app_manager.RyuApp):
 
         actions = [parser.OFPActionOutput(out_port), parser.OFPActionOutput(self.snort_port)]
 
-        # install a flow to avoid packet_in next time
-        if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
-            # verify if we have a valid buffer_id, if yes avoid to send both
-            # flow_mod & packet_out
-            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-                return
-            else:
-                self.add_flow(datapath, 1, match, actions)
+        # install a flow to avoid packet_in next time (TURNED OFF FOR NOW)
+        # if out_port != ofproto.OFPP_FLOOD:
+        #     match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+        #     # verify if we have a valid buffer_id, if yes avoid to send both
+        #     # flow_mod & packet_out
+        #     if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+        #         self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+        #         return
+        #     else:
+        #         self.add_flow(datapath, 1, match, actions)
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data

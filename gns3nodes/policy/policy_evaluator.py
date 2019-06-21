5bd3b1f1113612e672edfd2246cc410ac41cbe07
Kevin Hogan <kevin.hogan1992@gmail.com>
2019-06-21 17:33:31 -0400

    round trip firewall from malicious activity working (w/ hardcoding)

--- gns3nodes/policy/policy_evaluator.py
+++ gns3nodes/policy/policy_evaluator.py
@@ -1,11 +1,15 @@
 import sys
 import logging
+import requests
 from flask import Flask, request, render_template, json
 app = Flask(__name__)
 app.logger.setLevel(level=logging.INFO)
+
+CONTROLLER_IP = "10.0.0.3"
 
 @app.route('/ids_alert', methods = ['GET'])
 def config():
     #confJson = json.loads(json.dumps(request.json))
     print("Endpoint hit!")
+    requests.get(url = "http://" + CONTROLLER_IP + ":8080/firewall/block/192.168.0.5")
     return ""
--- gns3nodes/snort/src/alert_listener.py
+++ gns3nodes/snort/src/alert_listener.py
@@ -1,7 +1,10 @@
 from snortunsock import snort_listener
+import requests
 
 SOCKET_PATH = "/var/log/snort/snort_alert"
+POLICY_IP = "10.0.0.2"
 
 if __name__ == "__main__":
     for msg in snort_listener.start_recv(SOCKET_PATH):
         print('alertmsg: %s' % ''.join(msg.alertmsg[0].decode("utf-8").replace("\x00", "")))
+        requests.get(url = "http://" + POLICY_IP + ":8080/ids_alert")

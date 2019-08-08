8c07bd5985c286fd7c5c6a809a20c54954450378
Kevin Hogan <kevin.hogan1992@gmail.com>
2019-08-08 17:53:07 -0400

    src and dst IPs in alert listener

--- gns3nodes/ids/src/alert_listener.py
+++ gns3nodes/ids/src/alert_listener.py
@@ -1,10 +1,18 @@
 from snortunsock import snort_listener
 import requests
+import dpkt
+import socket
+import json
 
 SOCKET_PATH = "/var/log/snort/snort_alert"
 POLICY_IP = "10.0.0.2"
 
 if __name__ == "__main__":
     for msg in snort_listener.start_recv(SOCKET_PATH):
-        print('alertmsg: %s' % ''.join(msg.alertmsg[0].decode("utf-8").replace("\x00", "")))
-        requests.get(url = "http://" + POLICY_IP + ":8080/ids_alert")
+        eth = dpkt.ethernet.Ethernet(msg.pkt)
+        src_ip = socket.inet_ntop(socket.AF_INET, eth.data.src)
+        dst_ip = socket.inet_ntop(socket.AF_INET, eth.data.dst)
+        alert_msg = ''.join(msg.alertmsg[0].decode("utf-8").replace("\x00", ""))
+        alert_dict = {"msg": alert_msg, "src": src_ip, "dst": dst_ip}
+        print(json.dumps(alert_dict))
+        # requests.get(url = "http://" + POLICY_IP + ":8080/ids_alert")

ad71ec8458a376647f175c9f1ce61d240c7585d8
Kevin Hogan <kevin.hogan1992@gmail.com>
2019-08-08 13:55:38 -0400

    deleting all the unneeded stuff from the mininet iteration

--- webserver/reconfig/stack/LSTACK.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash -f
-
-# echo LAUNCHER $*
-ssh seamster ./lstack $*
-
-#    ... but some won't work until we open firewall ...
-# ssh vis ./lstack $*
--- webserver/reconfig/stack/xtern.h
+++
@@ -1,5 +0,0 @@
-/* external representation of stack */
-
-/* len of stack is in location 0 */
-int extern_s[STKSIZE+2];
-

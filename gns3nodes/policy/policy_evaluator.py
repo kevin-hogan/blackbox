726aeaf1c3f32d6a7cedbf8ea27b4fd36b036575
Kevin Hogan <kevin.hogan1992@gmail.com>
2019-08-13 14:47:05 -0400

    small refactor to use URI template

--- gns3nodes/policy/policy_evaluator.py
+++ gns3nodes/policy/policy_evaluator.py
@@ -1,16 +1,18 @@
 import sys
 import logging
 import requests
+from string import Template
 from flask import Flask, request, render_template, json
 app = Flask(__name__)
 app.logger.setLevel(level=logging.INFO)
 
 CONTROLLER_IP = "10.0.0.3"
+CONTROLLER_FIREWALL_URI = Template("http://" + CONTROLLER_IP + ":8080/firewall/block/$block_ip")
 PING_ALERT_MESSAGE = "ICMP PING"
 
 @app.route('/ids_alert', methods = ['POST'])
 def config():
     if PING_ALERT_MESSAGE in request.json["msg"]:
         print("Firewalling " + request.json["src"])
-        requests.get(url = "http://" + CONTROLLER_IP + ":8080/firewall/block/" + request.json["src"])
+        requests.get(url = CONTROLLER_FIREWALL_URI.substitute(block_ip=request.json["src"]))
     return ""
